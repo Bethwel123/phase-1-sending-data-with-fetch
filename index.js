584711c3077814deea1377ebddf6ccc11443e399
Bethwel123 <bethwellangat654@gmail.com>
2024-07-10 01:50:51 +0300

    Handled sending of data to remote host

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      dogName: "Byron",
+      dogBreed: "Poodle",
+    }),
+  };
+
+fetch('http://localhost:3000/dogs', configurationObject)
+.then(res => res.json)
+.then(object => HTMLFormControlsCollection.log(object))
+.catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+});
+
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({ name: name, email: email })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const id = data.id;
+    const newId = document.createElement('p');
+    newId.textContent = `New User ID: ${id}`;
+    document.body.appendChild(newId);
+  })
+  .catch(error => {
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = `Error: ${error.message}`;
+    document.body.appendChild(errorMessage);
+  });
+}
